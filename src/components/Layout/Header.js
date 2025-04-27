adf769165fe82c965d92459a22d9a1c4583df34c
bornsrss14 <bornsrss14@gmail.com>
2025-04-27 10:57:51 -0600

    first commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+export const Header = () => {
+  return (
+    <div>
+      <header className="header">
+        <nav className="nav">
+          <ul className="nav-list">
+            <li>
+              <a href="#secc1">SECC#1</a>
+            </li>
+            <li>
+              <a href="#secc2">SECC#2</a>
+            </li>
+            <li>
+              <a href="#secc3">SECC#3</a>
+            </li>
+            <li>
+              <a href="#secc4">SECC#4</a>
+            </li>
+            <li>
+              <a href="#secc5">SECC#5</a>
+            </li>
+            <li>
+              <a href="#secc6">SECC#6</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Layout/HeroSection.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const HeroSection = () => {
+  return (
+    <>
+      <section class="hero">
+        <div class="hero-content">
+          <h1>Notaría Pública No. 9</h1>
+          <p>Comprometidos con tu seguridad jurídica y confianza.</p>
+          <a href="#contacto" class="hero-button">
+            Agenda tu cita
+          </a>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
