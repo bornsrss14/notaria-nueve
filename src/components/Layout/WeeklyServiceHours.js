2e92b857bf8bbde793bb23ec391e2063aab7199b
bornsrss14 <bornsrss14@gmail.com>
2025-05-06 21:54:13 -0600

    Se cambia la estructura de los dias laborales y se agrega el array de objetos de dias festivos, para poder mostrar si está cerrado o no

--- src/components/Layout/WeeklyServiceHours.js
+++ src/components/Layout/WeeklyServiceHours.js
@@ -3,77 +3,106 @@ import React from "react";
 
 <sectio></sectio>;
 export const WeeklyServiceHours = () => {
-  const horarioServicio = [
+  function getTodayDay() {
+    const days = [
+      "domingo",
+      "lunes",
+      "martes",
+      "miércoles",
+      "jueves",
+      "viernes",
+      "sabado",
+    ];
+
+    const now = new Date();
+    return days[now.getDay()];
+  }
+
+  const diasFestivos = [
     {
-      day: "Monday",
-      firstHourOpen: 8,
-      firstHourClose: 2,
-      secondHourOpen: 16,
-      secondHourClose: 18,
+      dateFeriado: "2025-12-25",
+      cerrado: true,
     },
     {
-      day: "Tuesday",
-      firstHourOpen: 8,
-      firstHourClose: 2,
-      secondHourOpen: 16,
-      secondHourClose: 18,
+      dateFeriado: "2025-05-10",
+      cerrado: true,
     },
     {
-      day: "Wednesday",
-      firstHourOpen: 8,
-      firstHourClose: 2,
-      secondHourOpen: 16,
-      secondHourClose: 18,
-    },
-    {
-      day: "Thursday",
-      firstHourOpen: 8,
-      firstHourClose: 2,
-      secondHourOpen: 16,
-      secondHourClose: 18,
-    },
-    {
-      day: "Friday",
-      firstHourOpen: 8,
-      firstHourClose: 2,
-      secondHourOpen: 16,
-      secondHourClose: 18,
-    },
-    {
-      day: "Saturday",
-      firstHourOpen: 9,
-      firstHourClose: 2,
-      secondHourOpen: null,
-      secondHourClose: null,
-    },
-    {
-      day: "Sunday",
-      firstHourOpen: null,
-      firstHourClose: null,
-      secondHourOpen: null,
-      secondHourClose: null,
+      dateFeriado: "2025-11-02",
+      rangos: [{ open: "10:00", close: "14:00" }],
     },
   ];
+  const horarios = [
+    {
+      day: "lunes",
+      rangos: [
+        { open: "08:00", close: "14:00" },
+        { open: "16:00", close: "18:00" },
+      ],
+    },
+    {
+      day: "martes",
+      rangos: [
+        { open: "08:00", close: "14:00" },
+        { open: "16:00", close: "18:00" },
+      ],
+    },
+    {
+      day: "miércoles",
+      rangos: [
+        { open: "08:00", close: "14:00" },
+        { open: "16:00", close: "18:00" },
+      ],
+    },
+    {
+      day: "jueves",
+      rangos: [
+        { open: "08:00", close: "14:00" },
+        { open: "16:00", close: "18:00" },
+      ],
+    },
+    {
+      day: "viernes",
+      rangos: [
+        { open: "08:00", close: "14:00" },
+        { open: "16:00", close: "18:00" },
+      ],
+    },
+    {
+      day: "sábado",
+      rangos: [
+        { open: "08:00", close: "14:00" },
+        { open: null, close: null },
+      ],
+    },
+    {
+      day: "domingo",
+      rangos: [
+        { open: null, close: null },
+        { open: null, close: null },
+      ],
+    },
+  ];
+
   return (
     <div>
       <section className="grid-hours">
-        {horarioServicio.map((horario, index) => (
-          <>
-            <p>{horario.day}</p>
-            <p className="morning-shift">
-              {horario.firstHourOpen
-                ? `${horario.firstHourOpen}AM -`
-                : "Cerrado"}
-              {horario.firstHourClose ? `${horario.firstHourClose}PM` : ""}
-            </p>
-            <p className="afternoon-shift">
-              {horario.secondHourOpen
-                ? `${horario.secondHourOpen}PM -`
-                : "Cerrado"}
-
-              {horario.secondHourClose ? `${horario.secondHourClose}PM` : ""}
-            </p>
-          </>
+        {/* //className="grid-hours */}
+        {horarios.map((day, index) => (
+          <div key={index} className="single-range-hour border">
+            <p>{day.day}</p>
+            {day.rangos.map((rango, index) => (
+              <div key={index}>
+                <p>
+                  {rango.open ? (
+                    `${rango.open} -`
+                  ) : (
+                    <span className="close-span">cerrado</span>
+                  )}
+                </p>
+              </div>
+            ))}
+          </div>
         ))}
       </section>
     </div>
